5e32623866881ff0f5b21f0eee8b60af055eb4fb
Oleg <oleg.ribchinskyy@gmail.com>
2017-11-01 14:26:12 +0200

    JavaLupsLesson1

---
+++ Practice/src/Loops.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+        int i =1;
+        int j =1;
+
+
+    while (i<10){
+        System.out.println();
+        while(j<10){
+            System.out.print("$");
+        j++;
+        }
+    i++;
+    j=1;
+    }
+
+
+
+    }
+}
